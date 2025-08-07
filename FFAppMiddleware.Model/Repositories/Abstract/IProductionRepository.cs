093eb5dc4eed60099dcb58d739797393657e0637
mta <m.tamazlicaru1959@gmail.com>
2025-08-07 23:22:34 +0300

    Adaugare repozitoriilor

---
+++ FFAppMiddleware.Model/Repositories/Abstract/IProductionRepository.cs
@@ -0,0 +1,14 @@
+﻿using FFAppMiddleware.Model.DataScheme;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFAppMiddleware.Model.Repositories.Abstract
+{
+    public interface IProductionRepository
+    {
+        List<ProductDescription> GetProductDescriptions();
+    }
+}
---
+++ FFAppMiddleware.Model/Repositories/Real/ProductionRepository.cs
@@ -0,0 +1,26 @@
+﻿using FFAppMiddleware.Model.DataScheme;
+using FFAppMiddleware.Model.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFAppMiddleware.Model.Repositories.Real
+{
+    public class ProductionRepository : IProductionRepository
+    {
+        private AdventureWorksDbContext _db;
+
+        public ProductionRepository()
+        {
+            _db = new AdventureWorksDbContext();
+        }
+
+        public List<ProductDescription> GetProductDescriptions()
+        {
+            List<ProductDescription> productDescriptions = _db.ProductDescriptions.OrderBy(pd => pd.Description).ToList();
+            return productDescriptions;
+        }
+    }
+}
