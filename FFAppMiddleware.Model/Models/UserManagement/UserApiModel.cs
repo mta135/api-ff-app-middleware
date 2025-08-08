9724b11dfa3f2ecec6f872f8e1b167c43d203c6c
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-08-08 08:17:07 +0300

    Aduagare repositoriu UserMangement

---
+++ FFAppMiddleware.Model/Models/UserManagement/UserApiModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFAppMiddleware.Model.Models.UserManagement
+{
+    public class UserApiModel
+    {
+        public long Id { get; set; }
+
+        public string UserLogin { get; set; }
+
+        public string UserPassword { get; set; }
+
+        public string Idnp { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+    }
+}
