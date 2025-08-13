a98dbb2a7fd94e6d07121d053520d2f6cf5ba7d7
mihaitamazlicaru1959 <mihaitamazlicaru1959@outlook.com>
2025-08-13 16:13:30 +0300

    Redenumire.

--- FFAppMiddleware.Model/Repositories/Abstract/IUserManagementRepository.cs
+++ FFAppMiddleware.Model/Repositories/Abstract/IUserManagementRepository.cs
@@ -1,14 +1,10 @@
 ﻿using FFAppMiddleware.Model.Models.UserManagement;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FFAppMiddleware.Model.Repositories.Abstract
 {
     public interface IUserManagementRepository
     {
         Task<List<RegisteredUsersApiModel>> RetrieveRegisteredUsers();
+
     }
 }
